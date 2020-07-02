94e06f485d0ac12c04654a58582d5c4f8ec90e6e
group-project-keeper <67711338+group-project-keeper@users.noreply.github.com>
2020-07-02 17:50:14 +0200

    first push to repository

---
+++ src/main/java/pl/test/projectforsignup/ProjectForSignupApplication.java
@@ -0,0 +1,13 @@
+package pl.test.projectforsignup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectForSignupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectForSignupApplication.class, args);
+	}
+
+}
